75449c4b2457731ff3b17cdb7ce5f85556fcbd7a
shivohum04 <shivohum04@gmail.com>
2024-02-02 17:08:04 +0530

    [shiv]ADD: added the code for promises

--- program.js
+++ program.js
@@ -1,12 +1,25 @@
-// UNDERSTANDING CALLBACKS and their implementattion 
+// PROMISES 
+// Creating a new Promise
+const myPromise = new Promise((resolve, reject) => {
+  const condition = true; // You can set this to false to test the rejection
 
-function greeting(name) {
-  alert('Hello ' + name);
-}
+  // Simulating some asynchronous operation
+  setTimeout(() => {
+    if (condition) {
+      resolve('Promise is resolved successfully.'); // Resolving the promise
+    } else {
+      reject('Promise is rejected.'); // Rejecting the promise
+    }
+  }, 2000); // 2 seconds delay to simulate async operation
+});
 
-function processUserInput(callback) {
-  var name = prompt('Please enter your name.');
-  callback(name);
-}
-
-processUserInput(greeting);
+// Using the Promise
+myPromise
+  .then((successMessage) => {
+    // successMessage is whatever we passed in the resolve() function above
+    console.log('Success: ' + successMessage);
+  })
+  .catch((errorMessage) => {
+    // errorMessage is whatever we passed in the reject() function above
+    console.log('Error: ' + errorMessage);
+  });
